728db89ccf8ab78a8dbb55b530d6406085acfb61
kshatta <70550600+kshatta@users.noreply.github.com>
2023-07-21 11:20:30 -0700

    Create getenterpriseEdge.py
    This python script will let you print the out of api call "portal/rest/enterprise/getEnterpriseEdges"
    
    We are getting edge information for one enterprise

---
+++ getenterpriseEdge.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+url = "https://<company>.velocloud.net/portal/rest/enterprise/getEnterpriseEdges"
+
+payload = "{\"enterpriseId\":1}"
+headers = {
+  'Authorization': 'token XXXX',
+  'Content-Type': 'text/plain'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+
+data = response.text
+
+
+#print("Type:", type(data))
+
+json_object = json.loads(data)
+
+#print("Type:", type(json_object))
+
+#The following line will print the object with ID 4
+print(json_object[4])
+
+#The previous code will print the whole dictionary object, if we want just the value for the key id we can type the following 
+print(json_object[4]['id'])
